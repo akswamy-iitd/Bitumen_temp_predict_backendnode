a25e932d9bd84ee0da64403d9840cfdbd1b1dad5
akswamy-iitd <akswamy.tempreproject@gmail.com>
2024-09-27 18:45:44 +0530

    Update prouser.controller.js

--- controllers/prouser.controller.js
+++ controllers/prouser.controller.js
@@ -37,8 +37,8 @@ ProUserController.signin = async (req, res, next) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      // secure: process.env.NODE_ENV === "production",
-      // sameSite: "None",
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "None",
       maxAge: 24 * 60 * 60 * 1000, // 1 day
     });
 
