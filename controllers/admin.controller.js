f9e7bef6cbc78b81d49cf3275aed6292dcf759b8
srijan008 <srijankumar008@gmail.com>
2024-09-29 21:54:12 +0530

    .

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -47,8 +47,8 @@ AdminController.signin = async (req, res) => {
 
         res.cookie('token', token, {
             httpOnly: true,
-            // secure: process.env.NODE_ENV === 'production', // secure=true only in production
-            // sameSite: 'None', // For cross-site cookies
+            secure: process.env.NODE_ENV === 'production', // secure=true only in production
+            sameSite: 'None', // For cross-site cookies
             maxAge: 24 * 60 * 60 * 1000 // 1 day
         });
         
